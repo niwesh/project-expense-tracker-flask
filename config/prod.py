6cd09672b85a432652ae8af357fb88451605c2db
Nivesh Mishra <44528117+niwesh@users.noreply.github.com>
2023-10-22 22:28:32 +0530

    Added some changes for heroku deployment

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,10 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-USERNAME = 'postgres'
-PASSWORD = 'Hello'
-DB_NAME = 'project_expense_tracker_db'
-SQLALCHEMY_DATABASE_URI = f'postgresql://{USERNAME}:{PASSWORD}@localhost/{DB_NAME}'
+# USERNAME = 'postgres'
+# PASSWORD = 'Hello'
+# DB_NAME = 'project_expense_tracker_db'
+# SQLALCHEMY_DATABASE_URI = f'postgresql://{USERNAME}:{PASSWORD}@localhost/{DB_NAME}'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import create_app, db  # from the app package __init__
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
+# if __name__ == '__main__':
+#     flask_app = create_app('prod')
+#     with flask_app.app_context():
+#         db.create_all()
+#     flask_app.run()
+
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
     flask_app.run()
