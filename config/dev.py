7a8f96866e4d2dc722cec4ae84b822ccd245f4b9
Nivesh Mishra <44528117+niwesh@users.noreply.github.com>
2023-11-04 01:30:42 +0530

    Update dev.py

--- config/dev.py
+++ config/dev.py
@@ -1,7 +1,7 @@
 DEBUG = True
 SECRET_KEY = 'topsecret'
-USERNAME = 'postgres'
-PASSWORD = 'Hello'
+USERNAME = '****'
+PASSWORD = '****'
 DB_NAME = 'project_expense_tracker_db'
 SQLALCHEMY_DATABASE_URI = f'postgresql://{USERNAME}:{PASSWORD}@localhost/{DB_NAME}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
